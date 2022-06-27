dca2f2a86992ffc98b239795b74bd1090c6be48d
Alaca Mustafa <97735752+mustafalaca@users.noreply.github.com>
2022-06-27 15:32:32 +0300

    Update __init__.py

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
 
-engine = create_engine("sqlite:///UrlRecords.db")
+engine = create_engine("sqlite:///UrlRecords.db", connect_args={'check_same_thread': False})
 if not database_exists(engine.url):
     create_database(engine.url)
 
