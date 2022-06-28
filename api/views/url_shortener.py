6f74244092479c9535798e1b5ab1c6bd883acade
Alaca Mustafa <97735752+mustafalaca@users.noreply.github.com>
2022-06-28 14:03:14 +0300

    Update url_shortener.py

--- api/views/url_shortener.py
+++ api/views/url_shortener.py
@@ -1,23 +1,25 @@
-from flask import jsonify, Blueprint, request, redirect
+from flask import jsonify, Blueprint, request, redirect, abort
 from api.controllers import *
 
 bp = Blueprint("url_shortener", __name__)
 
 
-@bp.route('/shorten_url', methods=['POST'])
+@bp.route('/api/shorten_url', methods=['POST'])
 def url_shortener():
     original_url = request.get_json()['original_url']
     shortened_url_data = url_shortener_processor(original_url)
-    return jsonify({'result': shortened_url_data})
+    return jsonify({'result': shortened_url_data})  # TODO localhost:5001/short_url
 
 
 @bp.route('/<short_url>', methods=['GET'])
 def redirect_to_shortened_url(short_url):
     url_to_go = redirect_to_domain(short_url)
-    return redirect(url_to_go)
+    if url_to_go:
+        return redirect(url_to_go)
+    return abort(400)
 
 
-@bp.route('/list_shortened_url', methods=['GET'])
+@bp.route('/api/list_shortened_url', methods=['GET'])
 def list_of_shortened_url():
     list_of_urls = list_of_urls_processor()
     return jsonify({'result': list_of_urls})
