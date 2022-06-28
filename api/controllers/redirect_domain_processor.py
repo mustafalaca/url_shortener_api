0a9cd56525f431ef03fedd282f960ca22739b601
Alaca Mustafa <97735752+mustafalaca@users.noreply.github.com>
2022-06-28 14:05:20 +0300

    Update redirect_domain_processor.py

--- api/controllers/redirect_domain_processor.py
+++ api/controllers/redirect_domain_processor.py
@@ -5,6 +5,8 @@ session = Session()
 
 def redirect_to_domain(short_url):
     url = session.query(UrlRecords).filter_by(short_url=short_url).first()
+    if url is None:
+        return False
     url.usage_counter = url.usage_counter + 1
     session.commit()
     return url.original_url
